7e97ac7b96df0695674bd8bb468cd6a816719560
siddhartha77 <77siddhartha@gmail.com>
2026-04-23 18:17:06 -0700

    More utilities for v1.4

--- utils.h
+++ utils.h
@@ -3,5 +3,9 @@
 
 void    myPStrToCStr(Str255 s);
 void    myCopyPStr(const Str255 s, Str255 t);
+void    myInsertInPStr(Str255 s,const Str255 insertStr,short offset);
+short   myFindFirstInPStr(const Str255 s,unsigned char c);
+void    myDeleteElementFromPStr(Str255 s,unsigned short index);
+void    myReplaceCharWithPStrInPStr(Str255 s, unsigned char c, const Str255 insertStr);
 
 #endif __UTILS_H__
